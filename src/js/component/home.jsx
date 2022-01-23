606b98bbd6aecc67888ee79f7c85297997fa8ebe
Kennythekaiju <kennethbandera@gmail.com>
2022-01-23 20:37:43 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [tasks, setTask] = useState([]);
+	const [input, setInput] = useState("");
+
+	const deleter = (task) => {
+		let filteredTasks = tasks.filter((item) => item != task);
+		setTask(filteredTasks);
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card">
+			<div className="card-body">
+				<h2 className="card-title">Todo List</h2>
+				<div>
+					<input
+						onChange={(e) => setInput(e.target.value)}
+						value={input}
+						placeholder="Add New Task"
+						onKeyDown={(e) => {
+							if (e.code === "Enter") {
+								e.preventDefault();
+
+								setTask([...tasks, e.target.value]);
+								setInput("");
+							}
+						}}
+					/>
+				</div>
+
+				<div>
+					<ul>
+						{tasks.map((item, i) => (
+							<li key={i}>
+								{item}{" "}
+								<i
+									onClick={() => deleter(item)}
+									className="fas fa-trash"></i>{" "}
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<div>
+					{" "}
+					{tasks.length === 0
+						? "No tasks, add a tasks"
+						: tasks.length + " " + "items left"}
+				</div>
+			</div>
 		</div>
 	);
 };
